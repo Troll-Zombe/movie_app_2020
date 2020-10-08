055f9105d0f3a2d5aaa627528faa8a927090875d
Troll-Zombe <0914buster@gmail.com>
2020-10-08 23:03:05 +0900

    https://Troll-Zombe.github.io/movie_app_2020

---
+++ src/routes/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import './Home.css';
+
+class Home extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+                } = await axios.get('https://yts-proxy.now.sh/list_mocies.json?sort_by=rating');
+        this.setState({ movies, isLoading: false });
+    };
+    componentDidMount() {
+        this.getMovies();
+    }
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map(movie => (
+                            <movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
+        )
+    }
+}
+export default Home;
