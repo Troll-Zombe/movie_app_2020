c32a50384599ad652313d53bf4173f93625489cf
Troll-Zombe <0914buster@gmail.com>
2020-10-09 01:48:55 +0900

    final2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from '../components/Movie';
 import './Home.css';
 
 class Home extends React.Component {
@@ -12,7 +13,7 @@ class Home extends React.Component {
             data: {
                 data: { movies },
             },
-                } = await axios.get('https://yts-proxy.now.sh/list_mocies.json?sort_by=rating');
+                } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
         this.setState({ movies, isLoading: false });
     };
     componentDidMount() {
@@ -28,17 +29,21 @@ class Home extends React.Component {
                     </div>
                 ) : (
                     <div className="movies">
-                        {movies.map(movie => (
-                            <movie
-                                key={movie.id}
-                                id={movie.id}
-                                year={movie.year}
-                                title={movie.title}
-                                summary={movie.summary}
-                                poster={movie.medium_cover_image}
-                            />
-                        ))}
-                    </div>
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+
                 )}
             </section>
         )
